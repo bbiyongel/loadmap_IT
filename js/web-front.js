67b96b0daa3db6ce5b818967b0af5ec98eb3e5a2
박우림 <park-woorim@bag-ulim-ui-MacBook-Pro-7.local>
2020-04-21 03:37:43 +0900

    Update Front-End View

--- js/web-front.js
+++ js/web-front.js
@@ -1,4 +1,5 @@
-const cardTitles = document.getElementsByClassName("title");
+const cardTitles = document.getElementsByClassName("cardTitle"),
+  linkBtns = document.getElementsByClassName("linkBtn");
 
 function showBoxes(event) {
   const cardView = event.target.parentNode.parentNode,
@@ -7,12 +8,21 @@ function showBoxes(event) {
   commentsBox.classList.toggle("deactivation");
 }
 
-
+function moveSite(event) {
+  const cardView = event.target.parentNode.parentNode,
+    commentsBox = cardView.childNodes[3],
+    link = commentsBox.childNodes[1];
+  location.href = link.href;
+}
 
 function init() {
   Array.from(cardTitles).forEach((cardTitle) => {
     cardTitle.addEventListener("click", showBoxes);
-  })
+  });
+
+  Array.from(linkBtns).forEach((linkBtn) => {
+    linkBtn.addEventListener("click", moveSite);
+  });
 }
 
 init();
